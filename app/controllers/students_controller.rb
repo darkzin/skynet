6c097e6bc7613d3dbbd61dc4b667b8b43b864935
darkzin <darkzin1021@gmail.com>
2013-07-03 18:31:23 +0900

    first commit.

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,22 @@
+class StudentsController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def delete
+  end
+
+  def update
+  end
+
+  def show
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Skynet::Application.config.secret_key_base = '8ea7844c991afa70f58412196c246fbc432d8e32fa3ad56ac5c0cc97a9c23ec241b6dc53b3caf8932195816d851f3fcb33885136f98c4e1d95cbff08ad11f78d'
