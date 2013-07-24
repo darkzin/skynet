ac7fa455703f6e8edc1e897472daed0d1723d9aa
darkzin <darkzin1021@gmail.com>
2013-07-24 15:13:19 +0900

    remove duplicate migration file.

--- db/migrate/20130717085800_add_input_data_and_model_paper_to_criterions.rb
+++
@@ -1,6 +0,0 @@
-class AddInputDataAndModelPaperToCriterions < ActiveRecord::Migration
-  def change
-    add_column :problems, :input_data, :text
-    add_column :problems, :model_paper, :text
-  end
-end
