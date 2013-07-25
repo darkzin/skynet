63f3bd5836ead6a37c0aed2f6dbfd9600b62c93b
darkzin <darkzin1021@gmail.com>
2013-07-25 14:32:02 +0900

    modify student model to rework devise. erase validatable, and add authentication_keys => student_number.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,20 +2,24 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_action :authenticate_student!
+  before_action :authenticate_student!, :print_params
 
   def authenticate_user!
     authenticate_student! || authenticate_professor!
   end
 
   def after_sign_in_path_for(resource_or_scope)
-    @courses = current_user.courses.all
+    @courses = current_student.courses.all
 
     if @courses.count == 0
       courses_path
     else
       course_path(@courses.first)
     end
+  end
+
+  def print_params
+    p params
   end
 
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -2,8 +2,9 @@ class Student < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
+  devise :authentication_keys => [:student_number]
   devise :database_authenticatable, :registerable,
-  :recoverable, :rememberable, :trackable, :validatable
+  :recoverable, :rememberable, :trackable
 
   #attr_accessible :email, :password, :password_confirmation, :student_number, :name, :phone_number
 
