c3073ef893799073ac8e66fd23c2914e5fcd3992
SungHong-Cho <lunaticbreeze@gmail.com>
2013-07-25 15:58:51 +0900

    add index.html.haml courses

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130723123225) do
+ActiveRecord::Schema.define(version: 20130724060037) do
 
   create_table "assignments", force: true do |t|
     t.string   "state"
@@ -44,6 +44,8 @@ ActiveRecord::Schema.define(version: 20130723123225) do
     t.integer  "score"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "input_data"
+    t.text     "model_paper"
   end
 
   create_table "deadlines", force: true do |t|
@@ -75,6 +77,7 @@ ActiveRecord::Schema.define(version: 20130723123225) do
     t.string   "category_type"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "file"
   end
 
   add_index "file_infos", ["category_id", "category_type"], name: "index_file_infos_on_category_id_and_category_type"
