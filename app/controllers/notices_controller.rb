f0221862b96562b1c20b4fa906845d5a468b5bdf
darkzin <darkzin1021@gmail.com>
2013-07-29 22:07:42 +0900

    add content of notices controller actions.

--- app/controllers/notices_controller.rb
+++ app/controllers/notices_controller.rb
@@ -1,8 +1,13 @@
+# -*- coding: utf-8 -*-
 class NoticesController < ApplicationController
   def index
+    @course = Course.find(params[:course_id])
+    @notices = @course.notices.all
   end
 
   def show
+    @course = Course.find(params[:course_id])
+    @notice = @course.notices.find(params[:id])
   end
 
   def new
@@ -11,14 +16,37 @@ class NoticesController < ApplicationController
   end
 
   def create
+    @course = Course.find(params[:course_id])
+    @notice = @course.notices.build(params[:notice])
+
+    if @notice.save
+      redirect_to course_notices_path, notice: "공지사항이 성공적으로 등록되었습니다."
+
+    else
+      redirect_to new_course_notice_path(@course), notice: "공지사항 등록에 실패하였습니다."
+    end
+
   end
 
   def edit
+    @course = Course.find(params[:course_id])
+    @notice = @course.notices.find(params[:id])
   end
 
   def update
+    @course = Course.find(params[:course_id])
+    @notice = @course.notices.update_attributes(params[:notice])
+
+    if @notice.save
+      redirect_to course_notices_path, notice: "공지사항이 성공적으로 등록되었습니다."
+
+    else
+      redirect_to new_course_notice_path(@course), notice: "공지사항 등록에 실패하였습니다."
+    end
   end
 
   def delete
+    @course = Course.find(params[:course_id])
+    @notice = @course.notices.find(params[:id])
   end
 end
