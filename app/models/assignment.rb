45471732ac9bbc2a693884cf692cee12dae55caa
darkzin <darkzin1021@gmail.com>
2013-07-29 20:11:51 +0900

    fix bug on assignment view, and add score column on assignments table.

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -2,4 +2,7 @@ class Assignment < ActiveRecord::Base
   belongs_to :student
   belongs_to :problem
   has_many :file_infos, as: :category, dependent: :destroy
+
+  accepts_nested_attributes_for :file_infos
+
 end
