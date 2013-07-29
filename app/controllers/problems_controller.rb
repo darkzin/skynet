1017c001fd29a0c975a8e03ddc3de943e6f6c599
darkzin <darkzin1021@gmail.com>
2013-07-29 14:21:05 +0900

    add problem_id column to criterions table and fix problem.new page.

--- app/controllers/problems_controller.rb
+++ app/controllers/problems_controller.rb
@@ -13,10 +13,10 @@ class ProblemsController < ApplicationController
   end
 
   def create
-    @problem = Problem.new(params[:problems])
-
+    @problem = Problem.new(params.require(:problem).permit!)
+p params[:problem_id]
     if @problem.save
-      redirect_to subject_path(params[:subject_id]), notice: "문제가 성공적으로 저장되었습니다."
+      redirect_to subject_path(@problem.subject_id), notice: "문제가 성공적으로 저장되었습니다."
     else
       redirect_to new_subject_problem_path(params[:subject_id]), notice: "저장에 실패하였습니다. 다시 시도 해주세요"
     end
