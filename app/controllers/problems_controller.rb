063b3bc54f105fa8bde952914ee26eb76d82b67c
darkzin <darkzin1021@gmail.com>
2013-08-23 08:02:58 +0900

    add 'go to subject' button to problems.new view, and after problem create complete, you can problem create again now.

--- app/controllers/problems_controller.rb
+++ app/controllers/problems_controller.rb
@@ -4,6 +4,7 @@ class ProblemsController < ApplicationController
   end
 
   def new
+    @subject = Subject.find(params.permit(:subject_id)[:subject_id])
   end
 
   def edit
@@ -14,11 +15,10 @@ class ProblemsController < ApplicationController
 
   def create
     @problem = Problem.new(params.require(:problem).permit!)
-p params[:problem_id]
     if @problem.save
-      redirect_to subject_path(@problem.subject_id), notice: "문제가 성공적으로 저장되었습니다."
+      redirect_to new_subject_problem_path(params[:subject_id]), notice: "문제를 성공적으로 저장했습니다. 다음 문제를 입력해주세요."
     else
-      redirect_to new_subject_problem_path(params[:subject_id]), notice: "저장에 실패하였습니다. 다시 시도 해주세요"
+      redirect_to new_subject_problem_path(params[:subject_id]), alert: "저장에 실패하였습니다. 다시 시도 해주세요"
     end
   end
 
