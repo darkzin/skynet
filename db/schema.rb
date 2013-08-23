dcf1fa815a54a29866383013716f234b5ba00d16
SungHong-Cho <lunaticbreeze@gmail.com>
2013-08-23 11:43:19 +0900

    aaa

--- db/schema.rb
+++ db/schema.rb
@@ -46,6 +46,8 @@ ActiveRecord::Schema.define(version: 20130822084727) do
     t.integer  "score"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "input_data"
+    t.text     "model_paper"
     t.integer  "problem_id"
   end
 
